eb3e13625dece7ffe71110bf8617c4d77c648b16
honguito42 <honguito42@ef98d7ee-4242-aa3b-1863-00b4a76b245e>
2011-01-16 15:39:08 +0000

    Cambio en el metodo añadirID para que gestione la direccion de memoria. Ahora este metodo recibira como parametro la direccion de memoria dispoible y devolvera la nueva direccion de memoria disponible despues de añadir la variable en la TS

--- 2010-2011/src/compilador/tablaSimbolos/TS.java
+++ 2010-2011/src/compilador/tablaSimbolos/TS.java
@@ -14,8 +14,10 @@ public class TS {
 		tabla = new Hashtable<String,Detalles>();
 	}
 	
-	public void añadeID(String id, Integer dir, Tipo tipo){
+	public Integer añadeID(String id, Integer dir, Tipo tipo){
 		tabla.put(id, new Detalles(id,dir,tipo));
+		Integer prim_dir_disponible = dir + tipo.getTamanyo();
+		return prim_dir_disponible;
 	}
 	
 	public Tipo getTipo(String id){
