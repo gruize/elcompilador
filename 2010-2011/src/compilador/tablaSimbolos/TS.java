463ecd79694b271cff258779398a1f669e43a7af
honguito42 <honguito42@ef98d7ee-4242-aa3b-1863-00b4a76b245e>
2011-01-16 15:13:56 +0000

    Nuevos paquetes incorporados en el paquete compilador: Gestor de errores y gestor de la tabla de simbolos

---
+++ 2010-2011/src/compilador/tablaSimbolos/TS.java
@@ -0,0 +1,49 @@
+package compilador.tablaSimbolos;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+import compilador.tablaSimbolos.tipos.Tipo;
+
+public class TS {
+	
+	private Hashtable<String,Detalles> tabla;
+	
+	public TS() {
+		tabla = new Hashtable<String,Detalles>();
+	}
+	
+	public void añadeID(String id, Integer dir, Tipo tipo){
+		tabla.put(id, new Detalles(id,dir,tipo));
+	}
+	
+	public Tipo getTipo(String id){
+		return tabla.get(id).getTipo();
+	}
+	
+	public Integer getDir(String id){
+		return tabla.get(id).getDir();
+	}
+		
+	public Boolean existeID(String id){
+		return tabla.containsKey(id);
+	}
+	
+	public Detalles getDetalles(String id){
+		return tabla.get(id);
+	}
+	 	
+	public void salidaTS(){
+		Set<String> clave = tabla.keySet();
+		Iterator<String> it = clave.iterator();
+		String clave_actual;
+		Detalles details;
+		while(it.hasNext()){
+			clave_actual = it.next();
+			details = tabla.get(clave_actual);
+			System.out.println(details.toString());				
+		}
+	}
+	
+}
