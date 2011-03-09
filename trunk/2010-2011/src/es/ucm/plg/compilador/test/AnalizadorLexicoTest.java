670f1ed27a6eb46e8f86e899a1353721987f5159
apj.ali <apj.ali@ef98d7ee-4242-aa3b-1863-00b4a76b245e>
2011-03-09 17:12:45 +0000

    Gestión de errores en el analizador léxico

--- trunk/2010-2011/src/es/ucm/plg/compilador/test/AnalizadorLexicoTest.java
+++ trunk/2010-2011/src/es/ucm/plg/compilador/test/AnalizadorLexicoTest.java
@@ -3,12 +3,11 @@ package es.ucm.plg.compilador.test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import junit.framework.TestCase;
 import es.ucm.plg.compilador.analizador_lexico.AnalizadorLexico;
-import es.ucm.plg.compilador.gestorErrores.GestorErrores;
+import es.ucm.plg.compilador.analizador_lexico.DatosToken;
 
 public class AnalizadorLexicoTest extends TestCase {
 
@@ -18,49 +17,11 @@ public class AnalizadorLexicoTest extends TestCase {
 
 	}
 
-//	public void test1() throws IOException {
-//
-//		String programa = "";
-//		BufferedReader br = new BufferedReader(new FileReader(new File(
-//				"test/test1/code.txt")));
-//		String linea = br.readLine();
-//		if (linea != null) {
-//			programa += linea;
-//			linea = br.readLine();
-//			while (linea != null) {
-//				programa += System.getProperty("line.separator");
-//				programa += linea;
-//				linea = br.readLine();
-//			}
-//		}
-//
-//		System.out.println("TEST 1");
-//		System.out.println();
-//		System.out.println("----código:");
-//		System.out.println();
-//		System.out.println(programa);
-//
-//		AnalizadorLexico lexico = new AnalizadorLexico(programa);
-//		ArrayList<String> tokens = new ArrayList<String>();
-//
-//		while (!lexico.isFin_programa()) {
-//			lexico.scanner();
-//			tokens.add(lexico.getToken_actual());
-//		}
-//
-//		System.out.println();
-//		System.out.println("----tokens generados:");
-//		System.out.println();
-//		System.out.println(tokens);
-//		System.out.println();
-//		System.out.println();
-//	}
-//
-	public void test2() throws IOException {
+	public void test1() throws Throwable {
 
 		String programa = "";
 		BufferedReader br = new BufferedReader(new FileReader(new File(
-				"test/test2/prueba_bien_1.txt")));
+				"test/test1ok.txt")));
 		String linea = br.readLine();
 		if (linea != null) {
 			programa += linea;
@@ -85,56 +46,10 @@ public class AnalizadorLexicoTest extends TestCase {
 			lexico.scanner();
 			tokens.add(lexico.getToken_actual());
 		}
-
-		if (GestorErrores.getErrores_propios().size() > 0) {
-			System.out.println("¡¡¡ERROR!!!");
-			System.out.println(GestorErrores.getErrores_propios().toString());
-		} else {
-			System.out.println();
-			System.out.println("----tokens generados:");
-			System.out.println();
-			System.out.println(tokens);
-			System.out.println();
-			System.out.println();
+		
+		for (DatosToken t : lexico.getTokens()) {
+			System.out.println(t.getToken());
 		}
 	}
-//	
-//	public void test4() throws IOException {
-//	
-//			String programa = "";
-//			BufferedReader br = new BufferedReader(new FileReader(new File(
-//					"test/test4/code.txt")));
-//			String linea = br.readLine();
-//			if (linea != null) {
-//				programa += linea;
-//				linea = br.readLine();
-//				while (linea != null) {
-//					programa += System.getProperty("line.separator");
-//					programa += linea;
-//					linea = br.readLine();
-//				}
-//			}
-//	
-//			System.out.println("TEST 4");
-//			System.out.println();
-//			System.out.println("----código:");
-//			System.out.println();
-//			System.out.println(programa);
-//	
-//			AnalizadorLexico lexico = new AnalizadorLexico(programa);
-//			ArrayList<String> tokens = new ArrayList<String>();
-//	
-//			while (!lexico.isFin_programa()) {
-//				lexico.scanner();
-//				tokens.add(lexico.getToken_actual());
-//			}
-//	
-//			System.out.println();
-//			System.out.println("----tokens generados:");
-//			System.out.println();
-//			System.out.println(tokens);
-//			System.out.println();
-//			System.out.println();
-//		}
 
 }
